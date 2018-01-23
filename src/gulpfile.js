85c168a27697bb024d3859b2f4013089fb2fc17a
Banchero <juan.banchero@atos.net>
2018-01-23 16:38:39 +0000

    add +

---
+++ src/gulpfile.js
@@ -0,0 +1,28 @@
+"use strict"
+
+//import modules
+
+var gulp = require('gulp');
+var conn = require('gulp-connect');
+var open = require('gulp-open');
+var browserify = require('browserify');
+var reactify = require('reactify');
+var source = require('vinyl-source-stream');
+var concat = require('gulp-concat');
+var lint = require('gulp-eslint');
+
+//configuration values
+var config = {
+    port: 5500,
+    baseUrl : 'http://localhost',
+    paths: {
+        html :  './src/*.html',
+        js:     './src/**/*.js',
+        indexJs:'./src/index.js',
+        css:    [
+            './node_modules/bootstrap/dist/css/bootstrap.min.css'
+        ],
+        dist :  './dist'
+       
+    }
+}
