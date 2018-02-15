8862f8a8ffae2d5fca979cd83c743243755b5afc
Banchero <juan.banchero@atos.net>
2018-02-15 14:11:55 +0000

    up

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,13 @@
 $ = jQuery = require("jquery");
 
-var App = console.log("Browserify is working");
+// var App = console.log("Browserify is working");
+// import libs 
+var React = require('react');
+var ReactDOM = require('react-dom');
 
-module.exports = App;
+//also ref to the component page
+var Home = require('./js/components/home/homepage.jsx');
+
+// module.exports = App;
+//reder de components in the app elememt
+ReactDOM.render(<Home/>, document.getElementById('app'));
